804b60e1239d26d8162fd567a9e8a48f313e6054
Avraz  Zebary <avrazebary@gmail.com>
2021-04-09 20:21:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ newHeader.setAttribute("id","victory");
 
 newHeader.innerHTML = "is the champion";
 
+<<<<<<< HEAD
+=======
+
+// let newHeader = document.createElement("h1");
+// newHeader.setAttribute("id","victory");
+>>>>>>> fa132efa9336fcee77703ceb4fb63dc4b47ecae4
